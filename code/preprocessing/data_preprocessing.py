9355c8f829295ea37cca530727d780a519641e95
Yelzagubei <feixy72@163.com>
2018-06-21 20:36:38 +0800

    wjw add some files

---
+++ code/preprocessing/data_preprocessing.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from pandas import DataFrame
+import pandas as pd
+import os, sys, json, csv, re
+
+sys.path.append("..")
+BASE_DIR = os.path.abspath('..')
+
+ORIGIN_DATA = os.path.join(BASE_DIR, "data", "cffex-host-info")
+PRE_DATA = os.path.join(BASE_DIR, "data", "pre_data")
+
+origin_dir = ORIGIN_DATA
+pre_dir = PRE_DATA
+f_list = os.listdir(origin_dir)
+
+for i in f_list:          ##每个log文件
+    if os.path.splitext(i)[1] == '.log':
+        file_name = os.path.join(output_dir, os.path.splitext(i)[0] + '.csv')
+        # if not os.path.exists(file_name):
+        #     os.makedirs(file_name)
+        data = {}
+        keys = []
+        with open(origin_dir+"/"+i, "r") as fp1:
+            origin_data = fp1.read()
+            origin_data_list = origin_data.split(']')[:-1]  #每一天的数据组成的list
+            data_list = []   #json dict list
+            for item in origin_data_list:
+                if(len(item) > 0 and item[-1] != ']'):
+                    item = item + ']'
+                hour_data_list = item[1:-1].split('}, ')[:-1]
+                #print(hour_data_list)
+                for hour_data in hour_data_list:
+                    if(len(hour_data) > 0 and item[-1] != '}'):
+                        hour_data = hour_data + '}'
+                    data_list.append(json.loads(hour_data))    #json list
+                    #print(hour_data)
+            print(data_list)
+            print(list(data_list[0].keys()))
+            data_dict = dict(zip(list(data_list[0].keys()), [[] for i in range(len(data_list[0].keys()))]))
+
+            for data_item in data_list:
+                for key, value in data_item.items():
+                    data_dict[key].append(value)
+
+            df = pd.DataFrame(data_dict)
+
+
+            # keys = data_list[0].keys()
+            # data = data.fromkeys(keys, [])
+            #
+            # if not os.path.exists(file_name):
+            #     with open(file_name,"w") as fp2:
+            #         w = csv.DictWriter(fp2,keys)
+            #         w.writeheader()
+            #         for data_dict in data_list:
+            #             w.writerow(data_dict)
+            #
+            #
+            #             for key in keys:
+            #                 data[key].append(data_dict[key])
