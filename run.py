2549ce4a9d320b54e650b739fa7671bf0636ccce
jhljx <1261542265@qq.com>
2018-07-30 20:46:41 +0800

    修改了run.py里面完整的生成处理数据流程

--- run.py
+++ run.py
@@ -28,20 +28,17 @@ def call_data_preprocessing_func(flag=False):
         predict_data = os.path.join(predict_data_dir, 'predict_data.csv')
 
         #处理原始告警数据
-        #data_preprocessing.process_alarm_data(os.path.join(raw_data_dir, 'cffex-host-alarm'), alarm_data_dir)
+        data_preprocessing.process_alarm_data(os.path.join(raw_data_dir, 'cffex-host-alarm'), alarm_data_dir)
         # 处理原始数据，将json格式的原始log文件数据解析为dataframe格式的csv文件数据
-        #data_preprocessing.process_raw_data(origin_data_dir, output_cffex_info_dir)
+        data_preprocessing.process_raw_data(origin_data_dir, output_cffex_info_dir)
         # 提取output_data中的cffex-host-info数据的时间、最大值、最小值数据，将每个主机按cpu、磁盘、mem等部件存入plot_data中
         #plot data实际上是用来生成分类器所需要的特征
-        # data_preprocessing.generate_plot_data(output_cffex_info_dir, plot_data_dir)
+        data_preprocessing.generate_plot_data(output_cffex_info_dir, plot_data_dir)
 
         # plot_data中部分数据存在23点数据缺失问题，对数据进行线性插值处理
-        # data_preprocessing.insert_missing_data(plot_data_dir, plot_data_dir) #测试一下
+        data_preprocessing.insert_missing_data(plot_data_dir, plot_data_dir) #测试一下
         # 将特征数据与告警数据match到一起，按照主机名和时间 左连接将告警事件match到对应的特征数据中
-        #data_preprocessing.generate_alarm_data(alarm_processed_file, node_alias_file, alarm_out_file)
-        # 将predict_data中各主机的特征数据独立存储成csv文件，供matlab画图使用。在feature_extraction之后才会生成predict_data
-        # 检查plot_data数据完整性
-        #data_preprocessing.check_completeness(plot_data_dir)
+        data_preprocessing.generate_alarm_data(alarm_processed_file, node_alias_file, alarm_out_file)
 
 #调用特征提取的函数
 def call_feature_extraction_func(flag=False):
@@ -51,14 +48,16 @@ def call_feature_extraction_func(flag=False):
         merged_data_file = os.path.join(predict_data_dir, "merged_data.csv")
         history_data_file = os.path.join(predict_data_dir, "history_data.csv")
         merged_history_file =os.path.join(predict_data_dir, "merged_history_data.csv")
-        #将每个主机的cpu、六个公共磁盘、内存的最大值、最小值作为特征，整合到同一个dataframe中，并将所有主机的dataframe拼接在一起，形成一个特征矩阵
 
-        # feature_extraction.generate_feature_by_hostname(plot_data_dir, predict_data)
+        #将每个主机的cpu、六个公共磁盘、内存的最大值、最小值作为特征，整合到同一个dataframe中，并将所有主机的dataframe拼接在一起，形成一个特征矩阵
+        feature_extraction.generate_feature_by_hostname(plot_data_dir, predict_data)
+
+        # 将predict_data中各主机的特征数据独立存储成csv文件，供matlab画图使用。在feature_extraction之后才会生成predict_data
         # generatesubplot实际上是用来获得Matlab画图的数据，获得了predict_data之后才可以执行这条语句
         data_preprocessing.generate_subplot_data(predict_data, subplot_data_dir)
-        # 实际上这里逻辑有些混乱，这个subplot是根据feature_extraction获得了特征数据之后，才可以生成这个subplot的数据
 
-        feature_extraction.generate_history_feature(plot_data_dir,history_data_file)
+        #获取时间序列的历史特征
+        #feature_extraction.generate_history_feature(plot_data_dir,history_data_file)
         # 将特征数据与告警数据match到一起，按照主机名和时间 左连接将告警事件match到对应的特征数据中
         #feature_extraction.generate_data_matrix_and_vector(history_data_file,alarm_file,merged_history_file)
 
@@ -76,7 +75,7 @@ def call_predict_model_func(flag=False):
 
 
 if __name__ == '__main__':
-    call_data_preprocessing_func()
+    call_data_preprocessing_func(flag=True)
     call_feature_extraction_func(flag=True)
     call_predict_model_func()
 
