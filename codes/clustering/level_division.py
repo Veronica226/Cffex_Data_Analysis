15ab9c78f933848eae73dcdcd4c0480ce7bee257
Veronic226 <137176672@qq.com>
2018-10-10 21:27:07 +0800

    apriori实现关联规则“ git commit -m apriori实现关联规则“

--- codes/clustering/level_division.py
+++ codes/clustering/level_division.py
@@ -179,7 +179,7 @@ def get_correlation_by_hostname(merged_data_file,hist_plot_dir,alarm_content_fil
             correlation_dict = {}
             all_alarm_content_list = []
             for hostname,df in all_df.groupby('hostname'):
-                print(hostname)
+                # print(hostname)
                 cor_list = []
                 alarm_content_list = []
                 len = df.shape[0]
@@ -198,7 +198,7 @@ def get_correlation_by_hostname(merged_data_file,hist_plot_dir,alarm_content_fil
                             if(row_next['content']!='0'):
                                 content_id_2 = int(row_next['content'])
                                 event_2 = int(row_next['event'])
-                                print('Yes')
+                                # print('Yes')
                                 if correlation_dict.get((content_id_1,event_1,content_id_2,event_2)) == None:
                                     correlation_dict[(content_id_1,event_1,content_id_2,event_2)] = 1
                                 else:
@@ -212,8 +212,8 @@ def get_correlation_by_hostname(merged_data_file,hist_plot_dir,alarm_content_fil
                 # if(cor_list != []):
                 #     print(cor_list)
                 #     generate_hist_plot(cor_list,hostname,hist_plot_dir)
-
-            cor_analysis(all_alarm_content_list)
+            print(all_alarm_content_list)
+            cor_analysis_2(all_alarm_content_list)
 
 
 
@@ -264,16 +264,7 @@ def generate_content_event_pair(correlation_dict,alarm_content_file):
     return df
 
 
-def load_data_set():
-    """
-    Load a sample data set (From Data Mining: Concepts and Techniques, 3th Edition)
-    Returns:
-        A data set: A list of transactions. Each transaction contains several items.
-    """
-    data_set = [['l1', 'l2', 'l5'], ['l2', 'l4'], ['l2', 'l3'],
-            ['l1', 'l2', 'l4'], ['l1', 'l3'], ['l2', 'l3'],
-            ['l1', 'l3'], ['l1', 'l2', 'l3', 'l5'], ['l1', 'l2', 'l3']]
-    return data_set
+
 
 
 def create_C1(data_set):
@@ -417,17 +408,152 @@ def generate_big_rules(L, support_data, min_conf):
 
 
 def cor_analysis(data_set):
-    L, support_data = generate_L(data_set, k=3, min_support=0.2)
-    big_rules_list = generate_big_rules(L, support_data, min_conf=0.7)
+    L, support_data = generate_L(data_set, k=3, min_support=0.5)
+    big_rules_list = generate_big_rules(L, support_data, min_conf=0.2)
     for Lk in L:
-        print("=" * 50)
-        print("frequent " + str(len(list(Lk)[0])) + "-itemsets\t\tsupport")
-        print("=" * 50)
-        for freq_set in Lk:
-            print(freq_set, support_data[freq_set])
+        if len(Lk) != 0:
+            print(Lk)
+            print("=" * 50)
+            print("frequent " + str(len(list(Lk)[0])) + "-itemsets\t\tsupport")
+            print("=" * 50)
+            for freq_set in Lk:
+                print(freq_set, support_data[freq_set])
+
     print("Big Rules")
     for item in big_rules_list:
         print(item[0], "=>", item[1], "conf: ", item[2])
+
+
+###########################################################################
+##################第二种（有先后顺序）#####################################
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+
+    C1.sort()
+    return list(map(frozenset, C1))
+
+#将候选集Ck转换为频繁项集Lk
+#D：原始数据集
+#Cn: 候选集项Ck
+#minSupport:支持度的最小值
+def scanD(D, Ck, minSupport):
+    #候选集计数
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if can not in ssCnt.keys(): ssCnt[can] = 1
+                else: ssCnt[can] += 1
+
+    numItems = float(len(D))
+    Lk= []     # 候选集项Cn生成的频繁项集Lk
+    supportData = {}    #候选集项Cn的支持度字典
+    #计算候选项集的支持度, supportData key:候选项， value:支持度
+    for key in ssCnt:
+        support = ssCnt[key] / numItems
+        if support >= minSupport:
+            Lk.append(key)
+        supportData[key] = support
+    return Lk, supportData
+
+#连接操作，将频繁Lk-1项集通过拼接转换为候选k项集
+def aprioriGen(Lk_1, k):
+    Ck = []
+    lenLk = len(Lk_1)
+    for i in range(lenLk):
+        L1 = list(Lk_1[i])[:k - 2]
+        L1.sort()
+        for j in range(i + 1, lenLk):
+            #前k-2个项相同时，将两个集合合并
+            L2 = list(Lk_1[j])[:k - 2]
+            L2.sort()
+            if L1 == L2:
+                Ck.append(Lk_1[i] | Lk_1[j])
+
+    return Ck
+
+def apriori(dataSet, minSupport = 0.5):
+    C1 = createC1(dataSet)
+    L1, supportData = scanD(dataSet, C1, minSupport)
+    L = [L1]
+    k = 2
+    while (len(L[k-2]) > 0):
+        Lk_1 = L[k-2]
+        Ck = aprioriGen(Lk_1, k)
+        print("ck:",Ck)
+        Lk, supK = scanD(dataSet, Ck, minSupport)
+        supportData.update(supK)
+        print("lk:", Lk)
+        L.append(Lk)
+        k += 1
+
+    return L, supportData
+
+#生成关联规则
+#L: 频繁项集列表
+#supportData: 包含频繁项集支持数据的字典
+#minConf 最小置信度
+def generateRules(L, supportData, minConf=0.2):
+    #包含置信度的规则列表
+    bigRuleList = []
+    #从频繁二项集开始遍历
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if (i > 1):
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+
+
+# 计算是否满足最小可信度
+def calcConf(freqSet, H, supportData, brl, minConf=0.2):
+    prunedH = []
+    #用每个conseq作为后件
+    for conseq in H:
+        # 计算置信度
+        conf = supportData[freqSet] / supportData[freqSet - conseq]
+        if conf >= minConf:
+            print(freqSet - conseq, '-->', conseq, 'conf:', conf)
+            # 元组中的三个元素：前件、后件、置信度
+            brl.append((freqSet - conseq, conseq, conf))
+            prunedH.append(conseq)
+
+    #返回后件列表
+    return prunedH
+
+
+# 对规则进行评估
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.2):
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = aprioriGen(H, m + 1)
+       # print(1,H, Hmp1)
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if (len(Hmp1) > 0):
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
+
+def cor_analysis_2(dataset):
+    L, support_data = apriori(dataset, minSupport=0.5)
+    for Lk in L:
+        if len(Lk) != 0:
+            print(Lk)
+            print("=" * 50)
+            print("frequent " + str(len(list(Lk)[0])) + "-itemsets\t\tsupport")
+            print("=" * 50)
+            for freq_set in Lk:
+                print(freq_set, support_data[freq_set])
+
+    big_rules_list = generateRules(L, support_data, minConf=0.2)
+    # print(big_rules_list)
+    # for item in big_rules_list:
+    #     print(item[0], "=>", item[1], "conf: ", item[2])
+
 
 
 
